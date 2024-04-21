070ac50c07631208aba9f348bfe087cef5090d45
Chenzs108 <chenzs108@outlook.com>
2024-04-21 22:17:31 +0100

    Feat: Add basic kernel definitions
    Co-authored-by: Liugw <liu.guowen@outlook.com>

---
+++ include/kernel/krnl.h
@@ -0,0 +1,27 @@
+/**
+ * Basic kernel configurations.
+ */
+
+#pragma once
+
+#include "kernel/util/metric.h"
+
+//! The size of the kernel in bytes.
+inline constexpr stl::size_t krnl_size {MB(1)};
+
+//! The address of the kernel image when it is loaded.
+inline constexpr stl::uintptr_t krnl_base {0xC0000000};
+
+enum class Privilege {
+    //! The kernel.
+    Zero = 0,
+
+    One = 1,
+    Two = 2,
+
+    //! Users.
+    Three = 3
+};
+
+//! Initialize the kernel.
+void InitKernel() noexcept;
