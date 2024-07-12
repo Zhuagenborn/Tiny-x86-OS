94573c5e41e1431a3707c2c6f88f317483f33f3a
Chenzs108 <chenzs108@outlook.com>
2024-07-12 16:26:02 +0100

    Feat: Add user-mode C++ STL libraries

---
+++ include/user/stl/cstdint.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace stl {
+
+using int8_t = signed char;
+using int16_t = signed short;
+using int32_t = signed int;
+using int64_t = signed long long;
+
+using uint8_t = unsigned char;
+using uint16_t = unsigned short;
+using uint32_t = unsigned int;
+using uint64_t = unsigned long long;
+
+using uint_least32_t = uint32_t;
+
+using byte = unsigned char;
+using size_t = unsigned int;
+using intptr_t = int;
+using uintptr_t = unsigned int;
+using ptrdiff_t = int;
+
+}  // namespace stl
